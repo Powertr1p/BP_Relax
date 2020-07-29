b4adf75498680718b58ed59993a30c5d1fb534e5
Powertr1p <sc2aru@gmail.com>
2020-07-29 21:23:45 +0300

    incapsulated detection and raycast in their own classes

---
+++ Assets/Scripts/PlayerInput/TapDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerInput
+{
+    public class TapDetector : MonoBehaviour
+    {
+        public event Action OnPlayerTap;
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnPlayerTap?.Invoke();
+            }
+        }
+    }
+}
