7ba6db96b89f38a49c15a1005fc6c9295604ab43
Powertr1p <sc2aru@gmail.com>
2020-07-29 20:09:08 +0300

    created movement for bubble

---
+++ Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _magnitude = 0.5f;
+        [SerializeField] private float _frequency = 20f;
+
+        private Vector3 position;
+
+        private void Start()
+        {
+            position = transform.position;
+        }
+
+        private void Update()
+        {
+            MoveUp();
+        }
+
+        private void MoveUp()
+        {
+            position += transform.up * (Time.deltaTime * _speed);
+            transform.position = position + transform.right * (Mathf.Sin(Time.time * _frequency) * _magnitude);
+        }
+    }
+}
