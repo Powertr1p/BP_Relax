04b155e35134329a6f3af118aad38678451e1979
Powertr1p <sc2aru@gmail.com>
2020-07-30 09:32:36 +0300

    bubbles now can destroys when they left from screen

--- Assets/Scripts/Bubbles/Bubble.cs
+++ Assets/Scripts/Bubbles/Bubble.cs
@@ -1,3 +1,4 @@
+using System;
 using Interfaces;
 using UnityEngine;
 
@@ -5,9 +6,24 @@ namespace Bubbles
 {
     public class Bubble : MonoBehaviour, IPoppable
     {
+        [SerializeField] private Camera _camera;
+        
+        private void Update()
+        {
+            if (!IsBubbleOnScreen())
+                Destroy(gameObject);
+        }
+
         public void Pop()
         {
             Destroy(gameObject);
+        }
+
+        private bool IsBubbleOnScreen()
+        {
+            Vector2 screenPoint = _camera.WorldToViewportPoint(transform.position);
+            bool isOnScreen = screenPoint.y < 1.1f;
+            return isOnScreen;
         }
     }
 }
