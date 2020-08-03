250a71291d3a2f960d55f1d95a57b900af80f17b
Powertr1p <sc2aru@gmail.com>
2020-08-03 20:26:34 +0300

    created spawner

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _bubblePrefab;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(Spawn());
+    }
+    
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(_bubblePrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
