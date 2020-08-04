40287ec663d8de5a187e4120bbae04067d63988a
Powertr1p <sc2aru@gmail.com>
2020-08-04 10:45:07 +0300

    refactored Movement.cs

--- Assets/Scripts/Bubbles/Movement.cs
+++ Assets/Scripts/Bubbles/Movement.cs
@@ -4,36 +4,37 @@ namespace Bubbles
 {
     public class Movement : MonoBehaviour
     {
-        [SerializeField] private float _speedMultiplier = 0.001f;
+        [SerializeField] private float _speedMultiplier = 0.01f;
         
         private float _speed;
         private float _magnitude;
         private float _frequency;
 
-        private Vector3 position;
+        private Vector3 _position;
 
         private void Start()
         {
-            position = transform.position;
+            _position = transform.position;
             GetRandomAttrubutes();
-            var randomScale = Random.Range(1f, 2.5f);
-            transform.localScale = new Vector3(randomScale, randomScale);
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             MoveUp();
         }
 
         private void MoveUp()
         {
-            position += Vector3.up * (Time.deltaTime * _speed);
-            transform.position = position + Vector3.right * (Mathf.Sin(Time.time * _frequency) * _magnitude);
+            _position += Vector3.up * (Time.deltaTime * _speed);
+            transform.position = _position + Vector3.right * (Mathf.Sin(Time.time * _frequency) * _magnitude);
             _speed += _speedMultiplier;
         }
 
         private void GetRandomAttrubutes()
         {
+            var randomScale = Random.Range(0.3f, 2.5f);
+            transform.localScale = new Vector3(randomScale, randomScale);
+            
             _frequency = Random.Range(0.2f, 1.5f);
             _speed = Random.Range(0.5f, 1f);
             _magnitude = Random.Range(0.5f, 1.5f);
