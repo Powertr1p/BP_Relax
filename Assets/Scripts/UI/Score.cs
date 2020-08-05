a1f7f525e34667a9c50ae59044b2cc09ef111b70
Powertr1p <sc2aru@gmail.com>
2020-08-05 21:34:54 +0300

    player score now displays on the screen

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PlayerInput;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class Score : MonoBehaviour
+    {
+        [SerializeField] private OnBubbleHitHandler _hitHandler;
+        [SerializeField] private TextMeshProUGUI _scoreText;
+
+        private int _score;
+
+        private void OnEnable()
+        {
+            _hitHandler.OnBubblePopped += UpdateScore;
+        }
+
+        private void Start()
+        {
+            DisplayScore();
+        }
+
+        private void UpdateScore(int score)
+        {
+            _score += score;
+            DisplayScore();
+        }
+
+        private void DisplayScore()
+        {
+            _scoreText.text = _score.ToString();
+        }
+
+        private void OnDisable()
+        {
+            _hitHandler.OnBubblePopped -= UpdateScore;
+        }
+    }
+}
