0f9da56db44c6ab91cb9e012457a2d220e8c1a69
Powertr1p <sc2aru@gmail.com>
2020-08-11 21:06:09 +0300

    incapsulated components in scene

--- Assets/Scripts/Core/GameOverHandler.cs
+++ Assets/Scripts/Core/GameOverHandler.cs
@@ -1,21 +1,42 @@
-﻿using UI.TimeCounter;
+﻿using PlayerInput;
+using UI.TimeCounter;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Core
 {
+    [RequireComponent(typeof(TimeCounter))]
     public class GameOverHandler : MonoBehaviour
     {
         [SerializeField] private GameObject _gameOverPanel;
-        [SerializeField] private TimeCounter _timeCounter;
+        [SerializeField] private TapRaycaster _raycaster;
+        
+        private TimeCounter _timeCounter;
+
+        private void Awake()
+        {
+            _timeCounter = GetComponent<TimeCounter>();
+        }
 
         private void OnEnable()
         {
             _timeCounter.OnTimeIsUp += ToggleGameOver;
         }
 
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void ToMainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
         private void ToggleGameOver()
         {
             _gameOverPanel.SetActive(true);
+            _raycaster.gameObject.SetActive(false);
         }
     
         private void OnDisable()
