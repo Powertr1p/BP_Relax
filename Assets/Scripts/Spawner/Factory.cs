6fc824584aaa4d4434529c8cf0905114e24242c3
Powertr1p <sc2aru@gmail.com>
2020-08-18 11:31:49 +0300

    refacored spawn nad factory logic

--- Assets/Scripts/Spawner/Factory.cs
+++ Assets/Scripts/Spawner/Factory.cs
@@ -14,6 +14,27 @@ namespace Spawner
       [SerializeField] private float _speedMultiplier = 0.2f;
       [SerializeField] private float _delayBeforeSpeedUp = 2.5f;
 
+      [Header("Bubble Parameters")] 
+      [SerializeField] private float _minSpeed = 0.3f;
+      [SerializeField] private float _maxSpeed = 1.5f;
+      
+      [Space(5f)] 
+      [SerializeField] private float _minFrequency = 0.2f;
+      [SerializeField] private float _maxFrequency = 1.5f;
+      
+      [Space(5f)] 
+      [SerializeField] private float _minMagnitude = 0.5f;
+      [SerializeField] private float _maxMagnitude = 1.1f;
+
+      [Space(5f)] 
+      [SerializeField] private float _minScale = 0.7f;
+      [SerializeField] private float _maxScale = 2.5f;
+
+      [Space(5f)] 
+      [SerializeField] private float _speedInSlowState = 4f;
+
+      public float SpeedInSlowState => _speedInSlowState;
+
       private void Start()
       {
          StartCoroutine(IncreaseSpeedMultiplier());
@@ -38,14 +59,14 @@ namespace Spawner
 
       private void SetRandomAttributes(Bubble bubble)
       {
-         var randomScale = Random.Range(0.7f, 2.5f);
+         var randomScale = Random.Range(_minScale, _maxScale);
          bubble.transform.localScale = new Vector3(randomScale, randomScale);
 
          if (bubble.TryGetComponent(out Movement movement))
          {
-            movement.Speed = Random.Range(0.3f, 1.5f) + _speedMultiplier;
-            movement.Frequency = Random.Range(0.2f, 1.5f);
-            movement.Magnitude = Random.Range(0.5f, 1.1f);
+            movement.Speed = Random.Range(_minSpeed, _maxSpeed) + _speedMultiplier;
+            movement.Frequency = Random.Range(_minFrequency, _maxFrequency);
+            movement.Magnitude = Random.Range(_minMagnitude, _maxMagnitude);
          }
       }
 
--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using Bubbles;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Spawner
 {
     public class Spawner : MonoBehaviour
     {
+        [Header("Spawn Parameters")]
         [SerializeField] private Factory _factory;
         [SerializeField] private float _minSpawnDelay = 0.1f;
         [SerializeField] private float _maxSpawnDelay = 0.5f;
         [SerializeField] private float _spawnRadius = 2f;
+        
+        [Header("SlowState Spawn Range(only Y) ")] 
+        [SerializeField] private float _minSlowStateBottomYPosition = -1;
+        [SerializeField] private float _maxSlowStateBottomYPosition = 2;
+        [SerializeField] private float _minSlowStateTopYPosition = 11;
+        [SerializeField] private float _maxSlowStateTopYPosition = 13;
 
         private void Start()
         {
@@ -21,7 +29,7 @@ namespace Spawner
               while (true)
               {
                   var bubble = _factory.GetCreatedBubble();
-                  bubble.transform.position = transform.position + new Vector3(Random.Range(-_spawnRadius, _spawnRadius), 0);
+                  bubble.transform.position = SetBubblePosition(0, 0);
 
                   yield return new WaitForSeconds(Random.Range(_minSpawnDelay, _maxSpawnDelay));
              }
@@ -29,20 +37,26 @@ namespace Spawner
 
          public void SpawnAdditionalBubblesForSlowState()
          {
-             var speed = 4f;
-             for (int i = 0; i < 10; i++)
+             
+             for (int i = 0; i < 20; i++)
              {
                  var bubble = _factory.GetCreatedBubble();
-                 bubble.transform.position = transform.position + new Vector3(Random.Range(-_spawnRadius, _spawnRadius), Random.Range(-1,2));
-                 bubble.GetComponent<Movement>().Speed = speed;
+                 if (i <= 10)
+                 {
+                     bubble.transform.position = SetBubblePosition(_minSlowStateBottomYPosition, _maxSlowStateBottomYPosition);
+                     bubble.GetComponent<Movement>().Speed = _factory.SpeedInSlowState;
+                 }
+                 else
+                 {
+                     bubble.transform.position = SetBubblePosition(_minSlowStateTopYPosition, _maxSlowStateTopYPosition);
+                     bubble.GetComponent<Movement>().Speed = -_factory.SpeedInSlowState;
+                 }
              }
-
-             for (int i = 0; i < 10; i++)
-             {
-                 var bubble = _factory.GetCreatedBubble();
-                 bubble.transform.position = transform.position + new Vector3(Random.Range(-_spawnRadius, _spawnRadius), Random.Range(11,13));
-                 bubble.GetComponent<Movement>().Speed = -speed;
-             }
+         }
+         
+         private Vector2 SetBubblePosition(float minYPosition, float maxYPosition)
+         {
+             return transform.position + new Vector3(Random.Range(-_spawnRadius, _spawnRadius), Random.Range(minYPosition, maxYPosition));
          }
     }
 }
