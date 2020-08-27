61c622e7b9376f2a2d5551918e2d896c2533dff4
Powertr1p <sc2aru@gmail.com>
2020-08-27 13:05:05 +0300

    ads now playing when press start button

--- Assets/Scripts/Ads/RegularAds.cs
+++ Assets/Scripts/Ads/RegularAds.cs
@@ -1,7 +1,16 @@
+using System;
+using UnityEngine.Advertisements;
+
 namespace Ads
 {
     public class RegularAds : UnityAds
     {
         private const string _placementID = "video";
+        
+        public static void ShowAds()
+        {
+            if (Advertisement.IsReady(_placementID))
+                Advertisement.Show(_placementID);
+        }
     }
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,20 +1,46 @@
-using System;
 using Ads;
 using UnityEngine;
+using UnityEngine.Advertisements;
 using UnityEngine.SceneManagement;
 
 namespace UI
 {
-    public class MainMenu : MonoBehaviour
+    public class MainMenu : MonoBehaviour, IUnityAdsListener
     {
         private void Start()
         {
+            Advertisement.AddListener(this);
             StartCoroutine(BannerAds.ShowBanner());
         }
 
-        public void LoadGame()
+        private void LoadGame()
         {
-            SceneManager.LoadScene("Game");
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        public void ShowAdsAndLoadGame()
+        {
+            RegularAds.ShowAds();
+        }
+
+        public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+        {
+            if (showResult == ShowResult.Finished)
+                LoadGame();
+            else if (showResult == ShowResult.Skipped)
+                LoadGame();
+        }
+        
+        public void OnUnityAdsReady(string placementId)
+        {
+        }
+
+        public void OnUnityAdsDidError(string message)
+        {
+        }
+
+        public void OnUnityAdsDidStart(string placementId)
+        {
         }
     }
 }
