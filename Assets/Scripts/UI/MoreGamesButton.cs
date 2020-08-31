f699b20c8ff9be56c8e1e07555aacefea9a3dc55
Powertr1p <sc2aru@gmail.com>
2020-08-31 12:37:36 +0300

    more games button now randomly pick games from array

--- Assets/Scripts/UI/MoreGamesButton.cs
+++ Assets/Scripts/UI/MoreGamesButton.cs
@@ -4,9 +4,16 @@ namespace UI
 {
    public class MoreGamesButton : MonoBehaviour
    {
+      private string[] _appLinks =
+      {
+         "details?id=com.Bezrukov.TearIt", 
+         "details?id=com.IJunior.TrafficGame",
+         "details?id=com.IJunior.FindAPairLanguage"
+      };
+      
       public void OpenStore()
       {
-         Application.OpenURL("market://details?id=5525032069872992445");
+         Application.OpenURL($"market://{_appLinks[Random.Range(0, _appLinks.Length)]}");
       }
    }
 }
