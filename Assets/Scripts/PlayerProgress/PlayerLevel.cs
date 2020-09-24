bf638d36d79346996f0d3134f68d2cf0bdc5d1fb
Powertr1p <sc2aru@gmail.com>
2020-09-24 17:33:38 +0300

    added player progression logic

---
+++ Assets/Scripts/PlayerProgress/PlayerLevel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace PlayerProgress
+{
+    [RequireComponent(typeof(PlayerProgress))]
+    public class PlayerLevel : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _playerLevelText;
+
+        private PlayerProgress _playerProgress;
+        
+        private int _currentPlayerLevel;
+
+        private void Awake()
+        {
+            _playerProgress = GetComponent<PlayerProgress>();
+        }
+
+        private void OnEnable()
+        {
+            _playerProgress.OnFullLevelProgress += IncreasePlayerLevel;
+            _playerProgress.OnFullLevelProgress += UpdatePlayerLevelText;
+        }
+
+        private void Start()
+        {
+            _currentPlayerLevel = 1;
+            UpdatePlayerLevelText();
+        }
+    
+        private void IncreasePlayerLevel()
+        {
+            _currentPlayerLevel++;
+        }
+
+        private void UpdatePlayerLevelText()
+        {
+            _playerLevelText.text = $"Level {_currentPlayerLevel.ToString()}";
+        }
+
+        private void OnDisable()
+        {
+            _playerProgress.OnFullLevelProgress -= IncreasePlayerLevel;
+            _playerProgress.OnFullLevelProgress += UpdatePlayerLevelText;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerProgress/PlayerProgress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayerProgress
+{
+    public class PlayerProgress : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+
+        private float _currentPlayerLevelProgress;
+
+        public event Action OnFullLevelProgress;
+
+        public bool INC;
+        
+        private void Start()
+        {
+            _currentPlayerLevelProgress = 0;
+        }
+
+        private void Update()
+        {
+            if (INC)
+                IncreaseProgress();
+            INC = false;
+        }
+
+        private void IncreaseProgress()
+        {
+            _currentPlayerLevelProgress += 0.1f;
+            _slider.value = _currentPlayerLevelProgress;
+
+            if (_currentPlayerLevelProgress >= _slider.maxValue)
+            {
+                OnFullLevelProgress?.Invoke();
+                _currentPlayerLevelProgress = 0f;
+                _slider.value = 0f;
+            }
+                
+        }
+    }
+}
