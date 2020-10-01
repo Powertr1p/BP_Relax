fa8a67b3ad9072835ca682b83c1b9e2815680ff5
Powertr1p <sc2aru@gmail.com>
2020-10-01 14:52:29 +0300

    added level display on title screen

---
+++ Assets/Scripts/UI/MainMenuLevelProgressDisplay.cs
@@ -0,0 +1,25 @@
+﻿using PlayerProgress;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenuLevelProgressDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _currentLevelText;
+        [SerializeField] private Slider _progressSlider;
+
+        private void Start()
+        {
+            LoadPlayerData();
+        }
+
+        private void LoadPlayerData()
+        {
+            var loadedData = SavingSystem.LoadPlayer();
+            _currentLevelText.text = $"Level: {loadedData.PlayerLevel.ToString()}";
+            _progressSlider.value = loadedData.PlayerLevelProgress;
+        }
+    }
+}
